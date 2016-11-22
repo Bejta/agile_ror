443c4474d96e4ed80c0186748d96255b6482265a
Jasmin Bejtovic <jb223cp@student.lnu.se>
2016-11-22 08:47:39 +0000

    iteration B1: Unit testing of models finished

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+    fixtures :products
+    
     test "product attributes must not be empty" do
         product = Product.new
         assert product.invalid?
@@ -33,14 +35,18 @@ class ProductTest < ActiveSupport::TestCase
         image_url: image_url)
     end
     
-    test "image url" dook = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-      http://a.b.c/x/y/z/fred.gif }
-      bad = %w{ fred.doc fred.gif/more fred.gif.more }
-      ok.each do |name|
-          assert new_product(name).valid?, "#{name} shouldn't be invalid"
-      end
-      bad.each do |name|
-          assert new_product(name).invalid?, "#{name} shouldn't be valid"
-      end
+    test "image url" do
+              ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+              http://a.b.c/x/y/z/fred.gif }
+              
+              bad = %w{ fred.doc fred.gif/more fred.gif.more }
+              
+              ok.each do |name|
+                 assert new_product(name).valid?, "#{name} shouldn't be invalid"
+              end
+              bad.each do |name|
+                 assert new_product(name).invalid?, "#{name} shouldn't be valid"
+              end
     end
 end
+
