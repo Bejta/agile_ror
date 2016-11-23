33ebf6309dbdca93364c36998b32b67e854537eb
Jasmin Bejtovic <jb223cp@student.lnu.se>
2016-11-23 09:07:27 +0000

    iteration E1 - Creating a smarter cart...finished

--- depot/db/migrate/20161122141158_create_line_items.rb
+++ depot/db/migrate/20161122141158_create_line_items.rb
@@ -7,4 +7,5 @@ class CreateLineItems < ActiveRecord::Migration
       t.timestamps null: false
     end
   end
+  
 end
--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122152202) do
+ActiveRecord::Schema.define(version: 20161123083651) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
